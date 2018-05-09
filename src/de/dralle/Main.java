0c8651e1075d27444cc82611a77dc57281fc004a
Nils Dralle <nils.91@googlemail.com>
2018-05-09 15:12:04 +0200

    gitignore and main

---
+++ src/de/dralle/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.dralle;
+
+/**
+ * @author Nils
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
